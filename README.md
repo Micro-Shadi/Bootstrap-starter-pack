5bd52bfe7e64d8f1bc079092c3dcfea204b3c0b6
Micro-Shadi <micro.shadi@gmail.com>
2018-11-24 01:05:24 +0200

    ReadMe

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Bootstrap-starter-pack
-Bootstrap starter pack
+
+
+## Requirements
+
+To use this template, your computer needs:
+
+-	Ruby https://rubyinstaller.org/ 
+-	Node.js (0.12 or greater) Node.js is used to run the build processes. https://nodejs.org/en/download/ 
+	Test: run ` node -v ` in the terminal
+-	Npm (Node comes with npm installed so you should have a version of npm.) Used to manage development dependencies.
+	Test: run ` npm -v`  in the terminal
+-	Sass http://sass-lang.com/install 
+	Test: run ` sass -v`  in the terminal
+-	Gulp – task runner
+	`npm install -g gulp`
+	Test: run `gulp -v ` in the terminal
--- gulpfile.js
+++ gulpfile.js
@@ -8,9 +8,22 @@ const browserSync = require('browser-sync').create();
 const image = require('gulp-image');
 const cleanCSS = require('gulp-clean-css');
 const rename = require("gulp-rename");
+const sourcemaps = require('gulp-sourcemaps');
+
+// Move videos to dist/videos
+gulp.task('video', function(){
+  gulp.src('src/videos/*')
+    .pipe(gulp.dest('dist/videos'));
+});
+
+// Move Images to dist/images
+gulp.task('image', function(){
+  gulp.src('src/images/*')
+    .pipe(gulp.dest('dist/images'));
+});
 
 // Images compress
-gulp.task('image', function(){
+gulp.task('imageMin', function(){
   gulp.src('src/images/*')
     .pipe(image())
     .pipe(gulp.dest('dist/images'));
@@ -28,29 +41,39 @@ gulp.task('copyHtml', function(){
       .pipe(gulp.dest('dist'));
 });
 
+// Copy fonts to dist/css/fonts
+gulp.task('moveFonts', function(){
+  gulp.src('src/fonts/**/*.*')
+    .pipe(gulp.dest('dist/css/fonts'));
+});
+
 // Compile Sass
 gulp.task('sass', function(){
   return gulp.src('src/sass/*.scss')
-      .pipe(sass().on('error', sass.logError))
-      .pipe(gulp.dest('tmp/css'));
+      .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('tmp/css'))
+      .on('end', function(){ console.log('SASS to CSS is done.'); });
 });
 
 // PurgeCSS
 gulp.task('purgecss', function(){
   return gulp.src('tmp/css/*.css')
       .pipe(purgecss({
-        content: ['dist/*.html']
+        // whitelist: [],
+        content: ['dist/*.html', 'tmp/js/*.js']
       }))
-      .pipe(cleanCSS({compatiblity: 'ie8'}))
+      .pipe(cleanCSS({level: 2}))
       .pipe(rename("style.min.css"))
       .pipe(gulp.dest('dist/css'));
 });
 
 // Minify JS
 gulp.task('minifyjs', function(){
-  return gulp.src('src/js/*.js')
+  return gulp.src('src/js/**.js')
       .pipe(uglify())
-      .pipe(rename("main.min.js"))
+      // .pipe(rename("main.min.js"))
       .pipe(gulp.dest('tmp/js'));
 });
 
@@ -61,7 +84,7 @@ gulp.task('concat', function(){
       .pipe(gulp.dest('dist/js'));
 });
 
-// Static Server > watching SCSS/HTML/JS files
+// Static Server > watching SCSS/HTML/JS/Images/Fonts files
 gulp.task('serve', function() {
 
   browserSync.init({
@@ -71,11 +94,14 @@ gulp.task('serve', function() {
   gulp.watch('src/js/*.js', function(){ runSequence('minifyjs', 'concat')}).on('change', browserSync.reload);
   gulp.watch(['src/sass/*.scss', 'src/sass/**/*.scss'], function(){ runSequence('sass', 'purgecss')}).on('change', browserSync.reload);
   gulp.watch('src/*.html', function(){ runSequence('copyHtml', 'sass', 'purgecss')}).on('change', browserSync.reload);
+  gulp.watch('src/images/**.*', function(){ runSequence('image')}).on('change', browserSync.reload);
+  gulp.watch('src/fonts/**.*', function(){ runSequence('moveFonts')}).on('change', browserSync.reload);
+  gulp.watch('src/videos/**.*', function(){ runSequence('video')}).on('change', browserSync.reload);
 });
 
 /* gulp.task('default', [ 'copyHtml', 'sass', 'purgecss', 'minifyjs', 'concat']); */
 
 // build
 gulp.task('build', function() {
-  runSequence('bootstrap', 'copyHtml', 'image', 'sass', 'purgecss', 'minifyjs', 'concat');
+  runSequence('bootstrap', 'copyHtml', 'image', 'sass', 'purgecss', 'minifyjs', 'concat', 'video', 'moveFonts');
 });
